bad7f2d5b00ba82f376ba395b7a41a75e1fcf0e2
kuheli baidya <kuhelibaidya6@gmail.com>
2021-07-09 13:50:54 +0530

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,25 +1,19 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using API.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
 
 namespace API
 {
-    
+
     public class Startup
     {
         private readonly IConfiguration _config;
+
+        public object CompatibilityVersion { get; private set; }
 
         public Startup(IConfiguration config)
         {
@@ -38,25 +32,31 @@ namespace API
 
             });
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            });
+  
+            services.AddCors(options => options.AddPolicy("AllowAccess_To_API",policy=>policy.
+            AllowAnyOrigin().
+            AllowAnyMethod().
+            AllowAnyHeader()
+            ));
+           
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors("AllowAccess_To_API");
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+                
             }
 
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+        
 
             app.UseAuthorization();
 
---
+++ client/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'The Dating App';
+  user: any;
+
+  constructor(private http:HttpClient){
+    
+  }
+  ngOnInit(): void {
+    this.getUser();
+  
+  }
+  getUser(){
+    this.http.get('https://localhost:5001/api/user').subscribe(response =>{
+      this.user = response;
+  },error => {
+    console.log(error)
+  })
+  }
+}
